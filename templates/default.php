fc735e3195130fa5a4cdd797b3f71aeb3fd90738
BernhardBaumrock <office@baumrock.com>
2019-09-24 16:22:33 +0200

    move pagination to the left

--- templates/default.php
+++ templates/default.php
@@ -1,10 +1,10 @@
 <div class="RockTabulatorWrapper">
   <div class="header uk-margin-small-bottom uk-child-width-1-2@m" uk-grid>
-    <div class="gridactions"></div>
-    <div class="uk-text-center uk-text-right@m">
-      <span class="rowcount"></span>
+    <div class="uk-text-center uk-text-left@m">
       <span class="pagination"></span>
+      <span class="rowcount"></span>
     </div>
+    <div class="gridactions uk-text-right@m"></div>
   </div>
   <div class="RockTabulator">
     <?php
